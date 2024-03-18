89944035c1262019e408b1ac2190c19d60a78622
Kwinston752 <yalikhan752@gmail.com>
2024-03-18 21:46:14 +0600

    1.0

--- src/utils/checkIsTigrus.js
+++ src/utils/checkIsTigrus.js
@@ -1,5 +1,5 @@
 
 
 export const checkIsTigrus = () => {
-    return window.location.host.includes('tigrus.gscore.ru') || window.location.host.includes('localhost') || window.location.host.includes('test.gscore.ru');
+    return window.location.host.includes('tigrus.gscore.ru') || window.location.host.includes('testjonny.gscore.ru') || window.location.host.includes('localhost') || window.location.host.includes('test.gscore.ru');
 }
